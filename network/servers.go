32a9321489b351b4b2ecbdcbe4b695bde58c458e
Andrew Arrow <oneone@gmail.com>
2021-03-27 12:17:07 -0700

    loop

--- network/servers.go
+++ network/servers.go
@@ -19,12 +19,30 @@ func ShowServers(c *gin.Context) {
 }
 func AddServer(c *gin.Context) {
 	m := mapBody(c)
+	name := fmt.Sprintf("%s:%s", m["host"], m["port"])
+	in := ""
+	out := ""
 	ServerLock.Lock()
 	ServerMap[m["host"]] = m["port"]
 	ServerList = []string{}
 	for k, v := range ServerMap {
 		ServerList = append(ServerList, fmt.Sprintf("%s:%s", k, v))
 	}
+	index := 0
+	for i, s := range ServerList {
+		if s == name {
+			index = i
+			break
+		}
+	}
+	if index > 0 {
+		in = ServerList[index-1]
+	}
+	if index != len(ServerList)-1 {
+		out = ServerList[index+1]
+	} else if ServerList[0] != name {
+		out = ServerList[0]
+	}
 	ServerLock.Unlock()
-	c.JSON(200, gin.H{"ok": true})
+	c.JSON(200, gin.H{"in": in, "out": out})
 }
