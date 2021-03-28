ab374712abc80401c57935ddb30448eea63e33cc
Andrew Arrow <oneone@gmail.com>
2021-03-28 12:04:04 -0700

    methods

--- network/storage.go
+++ network/storage.go
@@ -8,13 +8,47 @@ import (
 const size = 64
 
 type Universe struct {
-	Base1   []uint64
-	Inboxes [][]*Timeline
+	Following []uint64
+	Inboxes   [][]*Timeline
+	Usernames map[string]byte
+	UserCount byte
+}
+
+func hasBit(n uint64, pos byte) bool {
+	val := n & (1 << pos)
+	return (val > 0)
+}
+
+func (u *Universe) BroadcastNewTimeline(t *Timeline) {
+	for i := byte(0); i < u.UserCount; i++ {
+		if u.ShouldDeliverFrom(t.From, i+1) {
+			u.Inboxes[i] = append([]*Timeline{t}, u.Inboxes[i]...)
+		}
+	}
+}
+
+func (u *Universe) ShouldDeliverFrom(username string, to byte) bool {
+	from := u.UsernameToIndex(username)
+	return hasBit(u.Following[to], from)
+}
+
+func (u *Universe) UsernameToIndex(username string) byte {
+	if u.UserCount == size {
+		return 0
+	}
+	if u.Usernames[username] == 0 {
+		u.UserCount++
+		u.Usernames[username] = u.UserCount
+	}
+	return u.Usernames[username]
 }
 
 func NewUniverse() *Universe {
 	u := Universe{}
-	u.Base1 = []uint64{}
+	u.Following = []uint64{}
+
+	u.Usernames = map[string]byte{}
+	u.UsernameToIndex("sysop")
 
 	t := Timeline{}
 	t.Text = "Welcome to CLT"
@@ -26,11 +60,11 @@ func NewUniverse() *Universe {
 	}
 	for i := 0; i < size; i++ {
 		// 18446744073709551615
-		u.Base1 = append(u.Base1, 0xFFFFFFFFFFFFFFFF)
+		u.Following = append(u.Following, 0xFFFFFFFFFFFFFFFF)
 		u.Inboxes = append(u.Inboxes, welcome)
 	}
 
-	fmt.Println(u.Base1)
+	fmt.Println(u.Following)
 	fmt.Println(u.Inboxes)
 	return &u
 }
