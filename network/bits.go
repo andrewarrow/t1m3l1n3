7f0f7af4f458ca0e37bc0e7fc1d51d227f89502c
Andrew Arrow <oneone@gmail.com>
2021-03-28 15:15:26 -0700

    bits

---
+++ network/bits.go
@@ -0,0 +1,75 @@
+package network
+
+type Bits uint64
+
+const (
+	F0 Bits = 1 << iota
+	F1
+	F2
+	F3
+	F4
+	F5
+	F6
+	F7
+	F8
+	F9
+	F10
+	F11
+	F12
+	F13
+	F14
+	F15
+	F16
+	F17
+	F18
+	F19
+	F20
+	F21
+	F22
+	F23
+	F24
+	F25
+	F26
+	F27
+	F28
+	F29
+	F30
+	F31
+	F32
+	F33
+	F34
+	F35
+	F36
+	F37
+	F38
+	F39
+	F40
+	F41
+	F42
+	F43
+	F44
+	F45
+	F46
+	F47
+	F48
+	F49
+	F50
+	F51
+	F52
+	F53
+	F54
+	F55
+	F56
+	F57
+	F58
+	F59
+	F60
+	F61
+	F62
+	F63
+)
+
+func Set(b, flag Bits) Bits    { return b | flag }
+func Clear(b, flag Bits) Bits  { return b &^ flag }
+func Toggle(b, flag Bits) Bits { return b ^ flag }
+func Has(b, flag Bits) bool    { return b&flag != 0 }
