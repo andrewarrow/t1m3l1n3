88be67e60687170a49a2c2273484c48260892d42
Andrew Arrow <oneone@gmail.com>
2021-04-02 10:45:34 -0700

    basic algo can be improved

--- network/message.go
+++ network/message.go
@@ -1,11 +1,37 @@
 package network
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 type Message struct {
 	Text string
 }
 
+func AsciiByteToBase9(a string) byte {
+
+	sum := byte(0)
+	for i := range a {
+
+		word := a[i : i+1]
+		t, _ := strconv.Atoi(word)
+
+		sum += byte(t)
+	}
+	strSum := fmt.Sprintf("%d", sum)
+	if len(strSum) > 1 {
+		return AsciiByteToBase9(strSum)
+	}
+	return sum
+
+}
+
 func (m *Message) Score() {
-	fmt.Println("Wefwe")
+	for _, a := range m.Text {
+		num := fmt.Sprintf("%d", a)
+		base9 := AsciiByteToBase9(num)
+		fmt.Printf("%d ", base9)
+	}
+	fmt.Printf("\n")
 }
--- network/message_test.go
+++ network/message_test.go
@@ -4,7 +4,11 @@ import "testing"
 
 func TestMessage(t *testing.T) {
 	m := Message{}
-	m.Text = "hi"
+	m.Text = "hello this message will be scored."
+	// 5 2 9 9 3 5 8 5 6 7 5 1 2 7 7 7 4 2 5 2 6 9 9 5 8 2 5 7 9 3 6 2 1 1
+	// f m 9 9 3 f f f 6 f f m m f f f m m f m 6 9 9 f f m f f 9 3 6 m m m
+	// f m 9 9 M f f f F f f m m f f f m m f m F 9 9 f f m f f 9 M F m m m
+	// . m - - m . . . . . . m m . . . m m . m . - - . . m . . - m . m m m
 
 	m.Score()
 	//if testJson != expected {
