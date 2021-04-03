841fdd01e9c487552b2aaed972d2503fcfe01476
Andrew Arrow <oneone@gmail.com>
2021-04-03 09:12:57 -0700

    threads

--- testing/messages/main.go
+++ testing/messages/main.go
@@ -15,8 +15,10 @@ func main() {
 	fmt.Println("messages")
 	f, _ := ioutil.ReadFile("../latin.html")
 	buff := []string{}
-	stats := map[int]int{}
-	stats2 := map[int]int{}
+	vg_more_m := []string{}
+	vg_more_f := []string{}
+	lg_more_m := []string{}
+	lg_more_f := []string{}
 	for _, line := range strings.Split(string(f), "\n") {
 		for _, word := range strings.Split(line, " ") {
 			buff = append(buff, word)
@@ -34,15 +36,35 @@ func main() {
 				fp := score["percent_f"].(float64)
 				gp := score["percent_g"].(float64)
 				sw := int(score["switches"].(float64))
-				stats[sw]++
 				lr := int(score["longest_run"].(float64))
-				stats2[lr]++
 				fmt.Printf("%s %.2f %.2f %.2f, %d %d\n\n", lf, mp, fp, gp, sw, lr)
+
+				if gp > 0.10 && mp > fp {
+					vg_more_m = append(vg_more_m, thing)
+				} else if gp > 0.10 && mp < fp {
+					vg_more_f = append(vg_more_f, thing)
+				} else if gp <= 0.10 && mp > fp {
+					lg_more_m = append(lg_more_m, thing)
+				} else if gp <= 0.10 && mp < fp {
+					lg_more_f = append(lg_more_f, thing)
+				}
 
 				buff = []string{}
 			}
 		}
 	}
-	fmt.Println(stats)
-	fmt.Println(stats2)
+
+	c := make(chan bool, 1)
+	go PostAs("bob", vg_more_m)
+	go PostAs("sue", vg_more_f)
+	go PostAs("mike", lg_more_m)
+	go PostAs("mary", lg_more_f)
+	<-c
+}
+
+func PostAs(name string, messages []string) {
+	for {
+		fmt.Println(name)
+		time.Sleep(time.Second * 10)
+	}
 }
